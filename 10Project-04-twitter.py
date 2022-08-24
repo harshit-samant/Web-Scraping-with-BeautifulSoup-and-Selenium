12304243ddbcd39c4fe4192ac2c87c5e924c7dc1
Harshit Samant <85729787+harshit-samant@users.noreply.github.com>
2022-08-24 11:15:32 +0530

    Update 10Project-04-twitter.py

--- 10Project-04-twitter.py
+++ 10Project-04-twitter.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-import chromedriver_autoinstaller
+#import chromedriver_binary
+#import chromedriver_autoinstaller
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -14,21 +15,27 @@ email = 'rajise7285@syswift.com'
 pswd = 'Sampleaccount'
 celebrity = 'Ryan Renolds'
 user_name = 'NamanRa92490947'
+tweets_length = 200
 
-chromedriver_autoinstaller.install()
-driver = webdriver.Chrome(service=Service())
+#hromedriver_autoinstaller.install()
+driver = webdriver.Chrome('C:/Users/harsh/Documents/Web Scraping/chromedriver.exe');
 
 driver.get(url)
+
 time.sleep(3)
+
 input_user = driver.find_element(by=By.CSS_SELECTOR, value='#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-kemksi.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div > div > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu.r-13qz1uu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input')
 input_user.send_keys(email)
 input_user.send_keys(Keys.ENTER)
 
 time.sleep(3)
 
-input_uName = driver.find_element(by=By.CSS_SELECTOR, value='#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-kemksi.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1dqxon3 > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input')
-input_uName.send_keys(user_name)
-input_uName.send_keys(Keys.ENTER)
+try:
+    input_uName = driver.find_element(by=By.CSS_SELECTOR, value='#layers > div > div > div > div > div > div > div.css-1dbjc4n.r-1awozwy.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-g6jmlv > div.css-1dbjc4n.r-1867qdf.r-1wbh5a2.r-kwpbio.r-rsyp9y.r-1pjcn9w.r-1279nm1.r-htvplk.r-1udh08x > div > div > div.css-1dbjc4n.r-kemksi.r-6koalj.r-16y2uox.r-1wbh5a2 > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1jgb5lz.r-1ye8kvj.r-13qz1uu > div.css-1dbjc4n.r-16y2uox.r-1wbh5a2.r-1dqxon3 > div > div.css-1dbjc4n.r-mk0yit.r-1f1sjgu > label > div > div.css-1dbjc4n.r-18u37iz.r-16y2uox.r-1wbh5a2.r-1wzrnnt.r-1udh08x.r-xd6kpl.r-1pn2ns4.r-ttdzmv > div > input')
+    input_uName.send_keys(user_name)
+    input_uName.send_keys(Keys.ENTER)
+except:
+    print("No username found")
 
 time.sleep(3)
 
@@ -36,15 +43,15 @@ input_pswd = driver.find_element(by=By.CSS_SELECTOR, value='#layers > div > div 
 input_pswd.send_keys(pswd)
 input_pswd.send_keys(Keys.ENTER)
 
-WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/label/div[2]/div/input')))
-
-search = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/label/div[2]/div/input')
+#WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input')))
+time.sleep(3)
+search = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[2]/div/div[2]/div/div/div/div[1]/div/div/div/form/div[1]/div/div/div/label/div[2]/div/input')
 search.send_keys(celebrity)
 search.send_keys(Keys.ENTER)
 
 time.sleep(3)
 
-people = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[2]/nav/div/div[2]/div/div[3]/a/div')
+people = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/div[1]/div[2]/nav/div/div[2]/div/div[3]/a/div')
 people.click()
 
 time.sleep(3)
@@ -68,9 +75,12 @@ while True:
     postings = soup.find_all('div', class_='css-901oao r-1nao33i r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0')
     
     tweets2 = list(set(tweets))
-    if len(tweets2) > 150:
+    if len(tweets2) > tweets_length:
         break
 
+df = pd.DataFrame({'Tweets': ['']})
+for tweet in tweets2:
+    df = df.append({'Tweets': tweet}, ignore_index=True)
 
-
+df.to_csv('C:/Users/harsh/Documents/Web Scraping/10Project-04--job tweets.csv')
     
